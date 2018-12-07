4a105aa4db232040407a404a9c35fa1ef096d032
Arjun Patel <45395213+arjunUpatel@users.noreply.github.com>
2018-12-07 09:27:37 -0500

    Create DiceHolder.java

---
+++ DiceHolder.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class DiceHolder{
+
+	Dice die=new Dice();
+	private ArrayList<Dice> holder=new ArrayList<>();
+
+	public DiceHolder(){
+	}
+	public int addDie(Dice nueva){
+
+		if(holder.size()<6){
+			holder.add(nueva);
+
+			return 1;
+		}
+
+		else{
+			return -1;
+		}
+
+	}
+
+	public void shake(){
+
+		for(int i=0;i<holder.size();i++){
+			holder.get(i).roll();
+		}
+
+	}
+		public String toString(){
+			for(int i=0;i<holder.size();i++){
+			System.out.println(holder.get(i));
+			}
+
+			return "";
+		}
+}
